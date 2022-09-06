39aa3732a3da753528cdb3b82788cd905726f1c2
seanhaythorne <seanhaythorne@hotmail.com>
2022-09-07 09:01:24 +1000

    Area growth surveillance design class template update

--- R/AreaGrowthSurvDesign.R
+++ R/AreaGrowthSurvDesign.R
@@ -1,10 +1,10 @@
 #' Area growth surveillance design class builder
 #'
 #' Builds a class to represent a surveillance design functionality for the
-#' effective allocation of surveillance sampling densities across one or more
-#' spatial sub-regions via a method that incorporates area-based population
-#' growth, surveillance and incursion management costs, detection
-#' sensitivities, and/or overall detection confidence.
+#' cost-effective allocation of surveillance sampling densities across one or
+#' more spatial sub-regions via a method that incorporates area-based
+#' population growth, surveillance and management costs, including costs
+#' associated with eradication, damages, and failure to contain incursions.
 #'
 #' @param context A \code{Context} or inherited class object representing the
 #'   context of a bio-security surveillance and area freedom design.
@@ -31,9 +31,6 @@
 #'   \code{pi*(growth_rate*size_class)^2}.
 #' @param sample_sens A vector of sample sensitivity values for each sub-region
 #'   specified by \code{divisions}. Default is \code{1}.
-#' @param optimal The strategy used for finding an effective sampling
-#'   allocation. One of (minimum) \code{"cost"} or (maximum) \code{"detection"}
-#'   sensitivity (up to \code{"confidence"} level when specified).
 #' @param mgmt_cost A list of vectors to represent estimated management costs
 #'   for population incursions. Each vector specifies costs at each sub-region
 #'   specified by \code{divisions}. List elements should be named
@@ -49,17 +46,14 @@
 #'   consistent with \code{sample_cost} when specified. Otherwise the units
 #'   should be consistent with the \code{surv_qty_unit} parameter specified in
 #'   the \code{context} (e.g. traps or samples).
-#' @param confidence The desired (minimum) system detection sensitivity or
-#'   confidence of the surveillance design (e.g. 0.95). Default is \code{NULL}.
 #' @param ... Additional parameters.
 #' @return A \code{AreaGrowthSurvDesign} class object (list) containing inherited
-#'   and extended functions from the \code{SamplingSurvDesign} class for
+#'   and extended functions from the base \code{SurveillanceDesign} class for
 #'   for allocating sample densities, and calculating (unit and overall)
 #'   detection sensitivities:
 #'   \describe{
 #'     \item{\code{get_allocation()}}{Get allocated sample densities via
-#'       specified strategy, utilizing costs, budget constraints, and/or
-#'       desired confidence level.}
+#'       specified strategy, utilizing costs and budget constraints.}
 #'     \item{\code{get_sensitivity()}}{Get the sub-region detection
 #'        sensitivities of the allocated surveillance design.}
 #'     \item{\code{get_confidence()}}{Get the overall system sensitivity or
@@ -83,12 +77,9 @@ AreaGrowthSurvDesign <- function(context,
                                    pi*(g*s)^2
                                  },
                                  sample_sens = 1,
-                                 optimal = c("cost", "detection"),
                                  mgmt_cost = list(),
                                  sample_cost = NULL,
-                                 budget = NULL,
-                                 confidence = NULL,
-                               class = character(), ...) {
+                                 budget = NULL, ...) {
   UseMethod("AreaGrowthSurvDesign")
 }
 
@@ -105,50 +96,31 @@ AreaGrowthSurvDesign.Context <- function(context,
                                            pi*(g*s)^2
                                          },
                                          sample_sens = 1,
-                                         optimal = c("cost", "detection"),
                                          mgmt_cost = list(),
                                          sample_cost = NULL,
-                                         budget = NULL,
-                                         confidence = NULL,
-                                         class = character(), ...) {
+                                         budget = NULL, ...) {
 
-  # Check divisions
-  if (!inherits(divisions, "Divisions")) {
-    stop(paste("Divisions parameter must be a 'Divisions' or inherited class",
-               "object."), call. = FALSE)
-  }
+  # Build via base class (for checks and system sensitivity)
+  self <- SurveillanceDesign(context = context,
+                             divisions = divisions,
+                             optimal = "cost",
+                             mgmt_cost = mgmt_cost,
+                             budget = budget,
+                             class = "AreaGrowthSurvDesign", ...)
 
   # Number of division parts
   parts <- divisions$get_parts()
 
-  # Check establish_rate
-  if (!is.numeric(establish_rate) || any(establish_rate < 0) ||
-      !length(establish_rate) %in% c(1, parts)) {
-    stop(paste("The establishment rate must be a numeric vector with",
-               "values  >= 0 for each subregion."), call. = FALSE)
-  }
-
-  # Build via base class (for checks and system sensitivity)
-  self <- SamplingSurvDesign(context = context,
-                             divisions = divisions,
-                             establish_pr = establish_rate, # relative
-                             sample_sens = sample_sens,
-                             sample_type = "continuous",
-                             design_dens = 0, # not used
-                             sample_area = 0, # not used
-                             optimal = "benefit", # avoid error
-                             mgmt_cost = list(),
-                             benefit = 0, # not used
-                             sample_cost = sample_cost,
-                             budget = budget,
-                             confidence = confidence,
-                             class = "AreaGrowthSurvDesign", ...)
-
-  # Check subregion_area, growth_rate, size_class_max, and class_pops_max
+  # Check parameters (not checked in base class)
   if (!is.numeric(subregion_area) || any(subregion_area < 0) ||
        !length(subregion_area) %in% c(1, parts)) {
     stop(paste("The sub-region area parameter must be a numeric vector with",
                "values  >= 0 for each sub-region."), call. = FALSE)
+  }
+  if (!is.numeric(establish_rate) || any(establish_rate < 0) ||
+      !length(establish_rate) %in% c(1, parts)) {
+    stop(paste("The establishment rate must be a numeric vector with",
+               "values  >= 0 for each subregion."), call. = FALSE)
   }
   if (!is.numeric(growth_rate) || any(growth_rate < 0) ||
       !length(growth_rate) %in% c(1, parts)) {
@@ -163,38 +135,22 @@ AreaGrowthSurvDesign.Context <- function(context,
     stop(paste("The maximum number of populations in a size class parameter",
                "must be numeric and >= 0."), call. = FALSE)
   }
-
-  # Check the area growth function
   if (!is.function(f_area_growth)) {
     stop("The area growth function must be a function.", call. = FALSE)
   }
 
-  # Match optimal arguments
-  optimal <- match.arg(optimal)
-
-  # Ensure relevant parameters are present for optimal strategy
-  if (optimal == "cost" && length(mgmt_cost) == 0) {
-    stop("The management cost parameter must be specified for optimal cost.",
-         call. = FALSE)
-  } else if (optimal == "detection" &&
-             (is.null(budget) && is.null(confidence))) {
-    stop(paste("Either the budget or confidence parameter must be specified",
-               "for optimal detection."), call. = FALSE)
-  }
-
   # Check mgmt_cost
-  if (!is.list(mgmt_cost) ||
-      !all(sapply(mgmt_cost, length) %in% c(1, parts))) {
-    stop(paste("The management cost parameter must be a list of numeric",
-               "vectors with values for each division part."), call. = FALSE)
-  } else if (optimal == "cost") {
-    if (!all(c("eradication", "damage", "penalty") %in% names(mgmt_cost))) {
-      stop(paste("The management cost parameter must contain list elements",
-                 "'eradication', 'damage', and 'penalty'."), call. = FALSE)
-    }
+  if (!all(c("eradication", "damage", "penalty") %in% names(mgmt_cost))) {
+    stop(paste("The management cost parameter must contain list elements",
+               "'eradication', 'damage', and 'penalty'."), call. = FALSE)
   }
 
-  # Resolve sample_cost
+  # Check and resolve sample_cost
+  if (!is.null(sample_cost) &&
+      (!is.numeric(sample_cost) || !length(sample_cost) %in% c(1, parts))) {
+    stop(paste("The sample cost parameter must be a numeric vector with ",
+               "values for each division part."), call. = FALSE)
+  }
   if (length(sample_cost) == 1) {
     sample_cost <- rep(sample_cost, parts)
   } else if (is.null(sample_cost)) {
@@ -212,11 +168,9 @@ AreaGrowthSurvDesign.Context <- function(context,
   #   pi*(g*s)^2
   # },
   # sample_sens = 1,
-  # optimal = c("cost", "detection"),
   # mgmt_cost = list(),
   # sample_cost = NULL,
-  # budget = NULL,
-  # confidence = NULL
+  # budget = NULL
 
   return(self)
 }
